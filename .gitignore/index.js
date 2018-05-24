ec5d595e161c31c9e5c065c712231015608c7474
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-24 18:24:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,3 +10,17 @@ bot.on("ready", () => {
 });
 
 bot.login(process.env.BOT_TOKEN);
+
+bot.on('message', message => {
+
+    if (message.content === prefix + "help") {
+        var embed = new Discord.RichEmbed()
+        .setTitle("Commande du ZBot !")
+        .addField("/help", "Affiche la liste des commandes du bot !")
+        .setFooter("© ZDown")
+        .setColor("0x2E9AFE")
+        .setTimestamp()
+        .setThumbnail(message.guild.iconURL)
+    message.channel.sendEmbed(embed)
+    }
+});
