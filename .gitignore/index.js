d3ae8736e7b6c17b5d5c233b12b0023c032d2317
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-26 18:26:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,26 +1,47 @@
+'use strict';
 const Discord = require('discord.js');
-const bot = new Discord.Client();
 
+
+var bot = new Discord.Client();
 var prefix = ("/");
 
-bot.on("ready", () => {
-  
-  console.log("Salut"); 
-  bot.user.setGame("/help | Dév' By NeYziX ✨")
+bot.on('ready', () => {
+    bot.user.setActivity("[/Help] Modération ZDown")
+    console.log("Bot Ready !");
 });
 
 bot.login(process.env.BOT_TOKEN);
 
-bot.on('message', message => {
+bot.on("guildMemberAdd", member => {
+    let role = member .guild.roles.find("name", "Membre");
+    member.guild.channels.find("name", "bienvenue").send(`${member}, bienvenue chez ZDown ! :heart:`)
+    member.addRole(role)
+})
 
-    if (message.content === prefix + "help") {
-        var embed = new Discord.RichEmbed()
-        .setTitle("Commande du ZBot !")
-        .addField("/help", "Affiche la liste des commandes du bot !")
-        .setFooter("© ZDown")
-        .setColor("0xd26cba")
-        .setTimestamp()
-        .setThumbnail(message.guild.iconURL)
-    message.channel.sendEmbed(embed)
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.find("name", "départ").send(`${member}, bon vent ! :joy:`)
+})
+
+bot.on('message', message => {
+    if (message.content === prefix + "ping"){
+        message.channel.sendMessage("Temps de latence avec le serveur : " + `${message.createdTimestamp - Date.now()}` + "MS");  
     }
+
+    if (message.content === prefix + "help"){
+        var embed = new Discord.RichEmbed()
+            .setColor('#24A493')
+            .setAuthor("Commandes du ZBot !", message.guild.iconURL)
+            .addField("/help", "Affiche la liste des commandes du bot !")
+        message.channel.sendEmbed(embed);
+        console.log("Commande Help demandée !");
+    }
+
+});
+
+bot.on('message', message => {
+    if (message.content === "quel est le travail de la mère a Manu ?"){
+        message.reply("La mère a Manu travail en tant que prostitué profesionelle avec des noir ayant une grosse teub");
+        console.log('Mere a Manu');
+    }
+
 });
